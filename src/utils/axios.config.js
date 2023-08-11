1827c441ad625addb64d7a7c96552c26eb568b59
Sabbir Hossen <webexpertsabbir@gmail.com>
2023-08-11 10:03:32 +0600

    axios 2

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,7 +1,24 @@
 import axios from "axios";
 
+let URL;
+
+switch (process.env.REACT_APP_DEVELOPMENT) {
+    case "DEVELOPMENT":
+        URL = "http://localhost:5000";
+        break;
+    case "PRODUCTION":
+        URL = "https://productionmontech.com";
+        break;
+    default:
+        URL = "http://localhost:5000";
+
+}
+
+
+
 const instance = axios.create({
-    baseURL: "http://localhost:5000",
+    // baseURL: "http://localhost:5000",
+    baseURL: URL,
 })
 
 export default instance;
